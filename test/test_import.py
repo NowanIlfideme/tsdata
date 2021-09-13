97daf9cb898fcd4d8ac77430ea33f90aabffe21e
NowanIlfideme <git@nowan.dev>
2021-09-13 17:28:10 +0200

    Added test for imports.

--- test/test_import.py
+++ test/test_import.py
@@ -4,3 +4,12 @@
 def test_import_basic():
     """This tests whether the `tsdata` package is importable."""
     import tsdata  # noqa
+
+
+def test_import_raw():
+    """This tests whether the `tsdata` package loads a dataset."""
+    from tsdata.raw import available_data, load_data
+
+    assert len(available_data()) > 0
+    assert "LakeHuron" in available_data()
+    load_data("LakeHuron")
