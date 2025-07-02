42dfde6ec8d86c214e981477b0d9a748a043cdcd
NowanIlfideme <git@nowan.dev>
2025-07-02 16:12:56 +0200

    Fixed tests.

--- test/test_import.py
+++ test/test_import.py
@@ -2,14 +2,22 @@
 
 
 def test_import_basic():
-    """This tests whether the `tsdata` package is importable."""
-    import tsdata  # noqa
+    """This tests whether the `tsdata` package is importable and can import a single dataset."""
+    from tsdata import fpp3  # noqa
+
+    assert len(fpp3.raw) > 0
+    assert "LakeHuron" in fpp3.raw
+    fpp3.raw["LakeHuron"]
 
 
-def test_import_raw():
-    """This tests whether the `tsdata` package loads a dataset."""
-    from tsdata.raw import available_data, load_data
+def test_import_all_raw():
+    """This tests whether the `tsdata` package loads all datasets."""
+    import pandas as pd
 
-    assert len(available_data()) > 0
-    assert "LakeHuron" in available_data()
-    load_data("LakeHuron")
+    from tsdata import fpp3, fpppy
+
+    for raw in [fpp3.raw, fpppy.raw]:
+        assert len(raw) > 0
+        for ds_name in raw:
+            df = raw[ds_name]
+            assert isinstance(df, pd.DataFrame)
