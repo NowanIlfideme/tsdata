1d099034b0b3976898613018ed5c54b502713453
NowanIlfideme <git@nowan.dev>
2025-07-02 16:07:42 +0200

    Moved all files and removed unused 'registry' stuff.

---
+++ src/tsdata/fpppy/__init__.py
@@ -0,0 +1,25 @@
+"""Loads data exported from Forecasting: Principles and Practice, The Pythonic Way.
+
+This module is automatically generated from available CSV files.
+
+See Also
+--------
+https://otexts.com/fpppy/
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+from tsdata._loader.as_pandas import PandasRawDatasetCollection
+
+raw = PandasRawDatasetCollection(dir=Path(__file__).parent)
+
+
+def __getattr__(name: str) -> pd.DataFrame:
+    if name in raw.keys():
+        return raw[name]
+    try:
+        return globals()[name]
+    except KeyError:
+        raise AttributeError(name)
